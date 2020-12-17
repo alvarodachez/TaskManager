f5c22c323d9357c92d3fb28256dc2fe217c27566
alvarodachez <sanchez99esp@gmail.com>
2020-12-17 11:04:19 +0100

    proyecto realizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ManagmentComponent } from './components/managment/managment.component';
+import { UsersComponent } from './components/users/users.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { InfoProjectComponent } from './components/info-project/info-project.component';
+import { InfoTaskComponent } from './components/info-task/info-task.component';
+import { InfoUserComponent } from './components/info-user/info-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ManagmentComponent,
+    UsersComponent,
+    ProjectsComponent,
+    InfoProjectComponent,
+    InfoTaskComponent,
+    InfoUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
